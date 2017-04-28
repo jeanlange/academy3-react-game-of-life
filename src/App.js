cf9c7ed6ff8459ae36c1f5b887473cd04e4745aa
Jean Lange <langeje@us.ibm.com>
2017-04-28 11:03:13 -0400

    move logicalGrid up to app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,34 @@ import './App.css';
 import Board from './Board';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.gridSideLength = 11;
+  }
+
+  logicalBoard() {
+    //construct logical grid filled with dead cells
+    var logicalGrid = [];
+    for(var rowNumber = 0; rowNumber < this.gridSideLength; rowNumber++) {
+      logicalGrid[rowNumber] = [];
+      for(var cellNumber = 0; cellNumber < this.gridSideLength; cellNumber++) {
+        logicalGrid[rowNumber][cellNumber] = false;
+      }
+    }
+
+    //set some cells alive
+    logicalGrid[5][4] =
+      logicalGrid[5][5] =
+      logicalGrid[5][6] =
+      true;
+
+    return logicalGrid;
+  }
+
   render() {
     return (      
       <div className="App">
-        <Board />
+        <Board logicalBoard={this.logicalBoard()}/>
       </div>
     );
   }
--- src/Board.js
+++ src/Board.js
@@ -4,27 +4,13 @@ import Cell from './Cell';
 class Board extends Component {
   render() {
     var grid = [];
-    var max = 11;
+    var logicalGrid = this.props.logicalBoard;
+    var gridSideLength = logicalGrid.length;
     var rowNumber, cellNumber;
 
-    //construct logical grid filled with dead cells
-    var logicalGrid = [];
-    for(rowNumber = 0; rowNumber < max; rowNumber++) {
-      logicalGrid[rowNumber] = [];
-      for(cellNumber = 0; cellNumber < max; cellNumber++) {
-        logicalGrid[rowNumber][cellNumber] = false;
-      }
-    }
-
-    //set some cells alive
-    logicalGrid[5][4] =
-      logicalGrid[5][5] =
-      logicalGrid[5][6] =
-      true;
-
     //construct the display grid based on logical grid
-    for(rowNumber = 0; rowNumber < max; rowNumber++) {
-      for(cellNumber = 0; cellNumber < max; cellNumber++) {
+    for(rowNumber = 0; rowNumber < gridSideLength; rowNumber++) {
+      for(cellNumber = 0; cellNumber < gridSideLength; cellNumber++) {
         grid.push(
           <Cell
             isAlive={logicalGrid[rowNumber][cellNumber]}
